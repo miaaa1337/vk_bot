dee553c005635fdcee39036f1c2930789bb72ad0
miaaa1337 <miaaaboravleva@gmail.com>
2026-09-01 16:51:01 -0700

    Update setup_group.py

--- setup_group.py
+++ setup_group.py
@@ -6,20 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 async def main():
-    # Берём твой токен группы из .env
     api = API(token=os.getenv("VK_TOKEN"))
     
     try:
-        # Вызываем правильный метод ВК для изменения настроек сообщества
         await api.request(
             "groups.setSettings",
             {
-                "group_id": 238512761, # ОБЯЗАТЕЛЬНО: впиши сюда цифры ID твоей группы!
+                "group_id": 238512761, 
                 "bots_capabilities": 1,
                 "bots_start_button": 0,
                 "bots_add_to_chats": 1,
                 "bots_allowed_to_add_chats": 1,
-                "bots_read_all_messages": 1 # ВОТ ОНА! Эта строчка включает доступ к переписке
+                "bots_read_all_messages": 1 
             }
         )
         print("=== НАСТРОЙКА ВКЛЮЧЕНА! БОТ ТЕПЕРЬ ВИДИТ МАТЫ ===")
